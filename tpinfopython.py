5ad468d1e4516d50ec2fccbe695fa073fb38e69c
Mgunther6 <112942492+Mgunther6@users.noreply.github.com>
2022-09-20 08:53:30 +0200

    Add files via upload

---
+++ tpinfopython.py
@@ -0,0 +1,74 @@
+ex1
+a=4
+carre=a**2
+print(« la valeur de a=,’a’, au carré est de, carré)
+
+ex2
+kilometre=100
+miles=kilometre/1,609
+print("La distance de ",kilometre," kilomètre est de ",miles," miles")
+
+
+ex3
+a=2
+b=3
+print("Avant échange a=",a," et b=",b)
+temp=a
+a=b
+b=temp
+
+
+print("Après échange a=",a," et b=",b)
+
+ex4
+valeur_tva=20
+
+prix_ht=40
+tva=prix_ht*valeur_tva/100
+prix_ttc=prix_ht+tva;
+    print("Prix HT du produit: ",prix_ht," €")
+    print("Indice de la TVA en 2015: ",valeur_tva," %")
+    print ("Valeur de la TVA: ",tva," €")
+    print("Prix TTC: ",prix_ttc," €")
+
+ex5
+a=3
+b=-15
+if a*b>0
+print("positif")
+elif print("négatif")
+
+ex6
+age=14
+if(age>=6)and(age<=7):
+    print("poussin")
+elif (age>=8) et (age<=9):
+    print("Pupille")
+elif(age>=10)and(age<=11)
+    print("minime")
+elif(age>=12)and(age<=15)
+    print("cadet")
+
+else:
+    ("hors catégorie")
+
+
+ex7
+nb_valeur=10
+result=0
+temp=0
+for k in range(1,nb_valeur+1):
+    temp=temp+1
+    result=result+temp
+
+print ("la somme des",nb_valeur,"premiers nombres entiers est",result)
+
+
+
+
+
+
+
+
+
+
